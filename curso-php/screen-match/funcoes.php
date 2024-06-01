b900b92de9a083273b6ba03d2cc70dc7a82ccc36
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2024-06-01 12:28:00 -0300

    commit 4

---
+++ curso-php/screen-match/funcoes.php
@@ -0,0 +1,16 @@
+<?php
+
+function exibeMesagemLancamento(int $ano): void {
+  if ($ano > 2022) {
+    echo "Esse filme é um lançamento" . PHP_EOL;
+  } elseif($ano > 2020 && $ano <= 2022) {
+    echo "Esse filme ainda é novo" . PHP_EOL;
+  } else {
+    echo "Esse filme não é um lançamento" . PHP_EOL;
+  }
+  
+}
+
+function incluidoNoPlano(bool $planoPrime, int $ano): bool {
+  return $planoPrime && $ano < 2020;
+}
